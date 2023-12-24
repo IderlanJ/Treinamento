489c0180f111e7bf0d83add8affe9f8b02778952
IderlanJ <iderlanblack9@gmail.com>
2023-12-24 13:36:36 -0300

    Atualizando sorteio

--- Sorteio.py
+++ Sorteio.py
@@ -1,16 +1,24 @@
 import random
 
-players = ['Bob','Beater','Aphelian','Nono','Rhyan','Indra','Siix','Zangado','Chacal','Look','Muca']
-sorteados = []
+players = ['Bob', 'Beater', 'Aphelian', 'Nono', 'Rhyan', 'Indra', 'Siix', 'Zangado', 'Chacal', 'Look', 'Muca', 'Kauan','Noelle','Carlos','Luis','Iderlan','Rafael','Lucas','Luciana','Marcos']
 
-while len(sorteados) < 5 and players:
-    sorteado = random.choice(players)
-    sorteados.append(sorteado)
-    players.remove(sorteado)
+# Verifica se há pelo menos 5 jogadores na lista
+if len(players) >= 5:
+    random.shuffle(players)  # Embaralha a lista original
 
-print('Os nomes sorteados foram:', sorteados)
+    num_lists = len(players) // 5  # Número de listas de 5 jogadores
+    rest = len(players) % 5  # Quantidade de jogadores restantes
 
+    for i in range(num_lists):
+        start = i * 5
+        end = start + 5
+        sorted_players = players[start:end]
+        print(f'Time {i + 1}:', sorted_players)
 
+    if rest > 0:
+        print(f'Jogadores restantes: {players[-rest:]}')
+else:
+    print('Não há jogadores suficientes para fazer o sorteio.')
 
 
 
